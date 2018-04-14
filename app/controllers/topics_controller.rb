e67af7c8f2f7fd93883d418ce4dec1f51d6d2ec9
Johan van Eck <johveck@gmail.com>
2018-04-14 20:07:28 +0200

    Some good progress on the topic#show.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,17 +2,24 @@ class TopicsController < ApplicationController
   before_action :verify_topic, only: :show
 
   def index
-    @topics = JSON.parse(Api::Topic.all)["topics"].map do |topic|
-      topic['type'] = 'card'
-      topic['stage'] = 'status: beslut'
-      OpenStruct.new(topic)
-    end
+    @topics = get_all_topics
     # TODO: Do something with the trending topics design thing
     @user_topic_subscriptions = TopicSubscription.where(user_id: cookies[:user_id])
   end
 
   def show
+    topics = get_all_topics
+    @topic = topics.select { |topic| topic.doc_id == @topic_id }.first
     @documents = Api::Document.all(@topic_id)
+    @user_topic_subscriptions = TopicSubscription.where(user_id: cookies[:user_id])
+  end
+
+  def get_all_topics
+    JSON.parse(Api::Topic.all)["topics"].map do |topic|
+      topic['type'] = 'card'
+      topic['stage'] = 'status: beslut'
+      OpenStruct.new(topic)
+    end
   end
 
   def verify_topic
