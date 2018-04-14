84a1a658713302e1219abbc03764d25d3f68da03
Stefan Wallin <stefan.w@festiz.com>
2018-04-15 01:08:53 +0200

    Endpoints for all region types

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,8 +4,23 @@ class RegionsController < ApplicationController
     render :index
   end
 
+  def cities_for_municipalities
+    @regions = Region.where(level: 2, parent_id: params[:municipality_id])
+    render :index
+  end
+
   def municipalities
     @regions = Region.where(level: 1)
+    render :index
+  end
+
+  def municipalities_for_county
+    @regions = Region.where(level: 1, parent_id: params[:county_id])
+    render :index
+  end
+
+  def county
+    @regions = Region.where(level: 0)
     render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'topics#index'
 
-  get '/municipalities.json', to: 'regions#municipalities'
-  get '/cities.json', to: 'regions#cities'
+  get '/regions/county', to: 'regions#county'
+  get '/regions/county/:county_id', to: 'regions#municipalities_for_county'
+  get '/regions/municipalities/', to: 'regions#municipalities'
+  get '/regions/municipalities/:municipality_id', to: 'regions#cities_for_municipalities'
+  get '/regions/cities', to: 'regions#cities'
+
   resources :topics, only: [:index, :show]
   resources :users, only: [:create, :destroy] do
     get 'regions_subscribed_to'
