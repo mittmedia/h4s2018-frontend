cbf1a5fa92b6ded45cdbfbe77a38348cf23c2770
Johan van Eck <johveck@gmail.com>
2018-04-15 09:04:28 +0200

    Make notifications actually work

--- app/javascript/packs/sw.js
+++ app/javascript/packs/sw.js
@@ -1,6 +1,12 @@
 self.addEventListener('push', (event) => {
   const eventData = event.data && event.data.json();
-  const title = (eventData.message) || 'You have received a notification';
-  const body = 'Please check the app to see the content';
-  event.waitUntil(self.registration.showNotification(title, { body }));
+  const title = 'Nytt remissvar';
+  const body = 'Polisfacket har nu svarat i frågan ‘Nästa steg på vägen mot  en mer jämlik hälsa - Förslag för ett långsiktigt arbete  för en god och jämlik hälsa';
+  event.waitUntil(self.registration.showNotification(title, { body, icon: '/android-chrome-192x192.png', data: eventData }));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  // This looks to see if the current is already open and
+  // focuses if it is
+  return event.waitUntil(clients.openWindow(`http://localhost:3000/topics/${event.notification.data.topic_id}`));
 });
